fe73a63620c254d3681b1076b988933164431b73
Kaneis Zontanos <ramim@bc.edu>
2024-11-19 15:59:17 -0500

    Initial Commit

---
+++ Task Tracker/Task_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_TrackerApp.swift
+//  Task Tracker
+//
+//  Created by Kaneis Zontanos on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Task_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
